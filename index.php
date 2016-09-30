5afb9eb2fae6ffb039c3ceef3c21370f516535be
BhasuraSuseen <bhasuras@live.com>
2016-09-30 20:24:34 +0530

    first Ui layout

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<link rel="stylesheet" type="text/css" href="styles/global.css" />
+<meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui"/>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+</head>
+<body> 
+	<div class="all_container">
+		<div class="left_side_bar">
+            <div class="left_side_bar_top">                   
+            </div>
+            <div class="left_side_bar_down">
+                <ul id="nav">
+                        <li id="open_hr_1" ><i> 
+                                <svg xmlns="http://www.w3.org/2000/svg" class="search-panel-query-icon" width="65px" height="50px" viewBox="0 0 70 60"  >
+                                    <g>
+                                        <path id="food_icon" d="M18.35,20.805c0.195,0.195,0.451,0.293,0.707,0.293c0.256,0,0.512-0.098,0.707-0.293c0.391-0.391,0.391-1.023,0-1.414
+                                            c-1.015-1.016-1.015-2.668,0-3.684c0.87-0.87,1.35-2.026,1.35-3.256s-0.479-2.386-1.35-3.256c-0.391-0.391-1.023-0.391-1.414,0
+                                            s-0.391,1.023,0,1.414c0.492,0.492,0.764,1.146,0.764,1.842s-0.271,1.35-0.764,1.842C16.555,16.088,16.555,19.01,18.35,20.805z"/>
+                                        <path id="food_icon" d="M40.35,20.805c0.195,0.195,0.451,0.293,0.707,0.293c0.256,0,0.512-0.098,0.707-0.293c0.391-0.391,0.391-1.023,0-1.414
+                                            c-1.015-1.016-1.015-2.668,0-3.684c0.87-0.87,1.35-2.026,1.35-3.256s-0.479-2.386-1.35-3.256c-0.391-0.391-1.023-0.391-1.414,0
+                                            s-0.391,1.023,0,1.414c0.492,0.492,0.764,1.146,0.764,1.842s-0.271,1.35-0.764,1.842C38.555,16.088,38.555,19.01,40.35,20.805z"/>
+                                        <path id="food_icon" d="M29.35,14.805c0.195,0.195,0.451,0.293,0.707,0.293c0.256,0,0.512-0.098,0.707-0.293c0.391-0.391,0.391-1.023,0-1.414
+                                            c-1.015-1.016-1.015-2.668,0-3.684c0.87-0.87,1.35-2.026,1.35-3.256s-0.479-2.386-1.35-3.256c-0.391-0.391-1.023-0.391-1.414,0
+                                            s-0.391,1.023,0,1.414c0.492,0.492,0.764,1.146,0.764,1.842s-0.271,1.35-0.764,1.842C27.555,10.088,27.555,13.01,29.35,14.805z"/>
+                                        <path id="food_icon" d="M25.345,28.61c0.073,0,0.147-0.008,0.221-0.024c1.438-0.324,2.925-0.488,4.421-0.488c0.004,0,0.008,0,0.013,0h0
+                                            c0.552,0,1-0.447,1-0.999c0-0.553-0.447-1.001-1-1.001c-0.004,0-0.009,0-0.014,0c-1.643,0-3.278,0.181-4.86,0.537
+                                            c-0.539,0.121-0.877,0.656-0.756,1.195C24.476,28.295,24.888,28.61,25.345,28.61z"/>
+                                        <path id="food_icon" d="M9.821,45.081c0.061,0.012,0.121,0.017,0.18,0.017c0.474,0,0.895-0.338,0.983-0.82c1.039-5.698,4.473-10.768,9.186-13.56
+                                            c0.475-0.281,0.632-0.895,0.351-1.37c-0.282-0.475-0.895-0.632-1.37-0.351c-5.204,3.083-8.992,8.661-10.134,14.921
+                                            C8.917,44.462,9.277,44.982,9.821,45.081z"/>
+                                        <path id="food_icon" d="M55.624,43.721C53.812,33.08,45.517,24.625,34.957,22.577c0.017-0.16,0.043-0.321,0.043-0.48c0-2.757-2.243-5-5-5
+                                            s-5,2.243-5,5c0,0.159,0.025,0.32,0.043,0.48C14.483,24.625,6.188,33.08,4.376,43.721C2.286,44.904,0,46.645,0,48.598
+                                            c0,5.085,15.512,8.5,30,8.5s30-3.415,30-8.5C60,46.645,57.714,44.904,55.624,43.721z M27.006,22.27
+                                            C27.002,22.212,27,22.154,27,22.098c0-1.654,1.346-3,3-3s3,1.346,3,3c0,0.057-0.002,0.114-0.006,0.172
+                                            c-0.047-0.005-0.094-0.007-0.14-0.012c-0.344-0.038-0.69-0.065-1.038-0.089c-0.128-0.009-0.255-0.022-0.383-0.029
+                                            c-0.474-0.026-0.951-0.041-1.432-0.041s-0.958,0.015-1.432,0.041c-0.128,0.007-0.255,0.02-0.383,0.029
+                                            c-0.348,0.024-0.694,0.052-1.038,0.089C27.1,22.263,27.053,22.264,27.006,22.27z M26.399,24.368
+                                            c0.537-0.08,1.077-0.138,1.619-0.182c0.111-0.009,0.222-0.017,0.333-0.025c1.098-0.074,2.201-0.074,3.299,0
+                                            c0.111,0.008,0.222,0.016,0.333,0.025c0.542,0.044,1.082,0.102,1.619,0.182c10.418,1.575,18.657,9.872,20.152,20.316
+                                            c0.046,0.321,0.083,0.643,0.116,0.965c0.011,0.111,0.026,0.221,0.036,0.332c0.039,0.443,0.068,0.886,0.082,1.329
+                                            c-15.71,3.641-32.264,3.641-47.974,0c0.015-0.443,0.043-0.886,0.082-1.329c0.01-0.111,0.024-0.221,0.036-0.332
+                                            c0.033-0.323,0.07-0.645,0.116-0.965C7.742,34.24,15.981,25.942,26.399,24.368z M30,55.098c-17.096,0-28-4.269-28-6.5
+                                            c0-0.383,0.474-1.227,2.064-2.328c-0.004,0.057-0.002,0.113-0.006,0.17C4.024,46.988,4,47.54,4,48.098v0.788l0.767,0.185
+                                            c8.254,1.98,16.744,2.972,25.233,2.972s16.979-0.991,25.233-2.972L56,48.886v-0.788c0-0.558-0.024-1.109-0.058-1.658
+                                            c-0.004-0.057-0.002-0.113-0.006-0.17C57.526,47.371,58,48.215,58,48.598C58,50.829,47.096,55.098,30,55.098z"/>
+                                    </g>                     
+                                    </svg> 
+                            </i>                               
+                        </li>
+                        <li> B  </li>
+                        <li> C  </li>
+                </ul>
+           </div>
+        </div>
+		<div class="container">
+            <div class="middle_top">
+			    <div class="middle_top_left">
+                    <h1>#<spanh1>OMS</spanh1></h1>
+                </div>
+                <div class="middle_top_middle">
+                <script type="text/javascript">
+                            tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+                            tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+                            function GetClock(){
+                            var d=new Date();
+                            var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
+                            if(nyear<1000) nyear+=1900;
+                            var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
+
+                            if(nhour==0){ap=" AM";nhour=12;}
+                            else if(nhour<12){ap=" AM";}
+                            else if(nhour==12){ap=" PM";}
+                            else if(nhour>12){ap=" PM";nhour-=12;}
+
+                            if(nmin<=9) nmin="0"+nmin;
+                            if(nsec<=9) nsec="0"+nsec;
+
+                            document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
+                            }
+
+                            window.onload=function(){
+                            GetClock();
+                            setInterval(GetClock,1000);
+                            }
+                            </script>
+                            <div id="clockbox"></div>
+                      </div>
+                      <div class="middle_top_right">
+                          <a>Bhasura Suseen</a>
+                      </div>
+            </div>
+			
+			
+            <div class="middle_left">
+                <a> art is the simplest imagination</a>
+            </div>
+            <div class="middle_right">
+                <button class="button button1">GA</button>
+            </div>
+		</div>
+		<div class="right_side_bar"> 
+            <svg xmlns="http://www.w3.org/2000/svg" class="search-panel-query-icon" width="24px" height="24px" viewBox="0 0 24 24"  >
+                <path fill="#455A64" d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+             </svg>
+             
+        </div>
+    <div id="pop_background"></div>
+    <div id="pop_box_hr_1">
+        <form name="new_user" action="admin.php" method="post" accept-charset="utf-8" onsubmit="return checkForm(this);">
+			<table>
+			<thead>
+			<tr>
+			<th colspan=2>Enter New User Details </th></thead>
+			</tr>
+			<tr><td>User Name</td><td> 
+			<input type="text" name="user_name" size="20" required></td></tr>
+			<tr><td>Password</td><td> 
+			<input type="password" name="password" size="20" required></td></tr>
+			<tr><td>Confirm Password</td><td> 
+			<input type="password" name="passwordc" size="20" required></td></tr>
+			<tr><td>NIC</td><td> 
+			<input type="text" name="nic" size="20" required></td></tr>
+			<tr><td>User Type</td><td><input type="radio" name="usertype" value="1"> Administrator
+  			<input type="radio" name="usertype" value="0" required=""> User</td></tr>
+			<tr><td colspan=2 align="center">
+			<input type="submit" value="Add new user" name="submit">
+			<input type="reset" value="Reset"></td>
+			</tr></table>
+			</form>
+    </div>
+	</div>
+    
+       <script>
+        $(document).ready(function(){
+           $('#open_hr_1').click(function(){
+               $('#pop_background').fadeIn();
+               $('#pop_box_hr_1').fadeIn();
+               return false;
+           });
+           $('#pop_background').click(function(){
+               $('#pop_background').fadeOut();
+               $('#pop_box_hr_1').fadeOut();
+
+               return false;
+           });
+        });
+    </script>
+</body>
+</html>
