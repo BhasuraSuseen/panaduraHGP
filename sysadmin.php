48dd10a0698cc844323d9e75ae0fce75c54e3f8e
BhasuraSuseen <bhasuras@live.com>
2016-10-04 05:26:49 +0530

    ms to AO date

--- sysadmin.php
+++ sysadmin.php
@@ -105,6 +105,26 @@
 			</tr></table>
 			</form>
     </div>
+    <div id="pop_box_mail_AE"><b>Enter MS to AO date</b>
+	<form name="mstoao" action="adminofficer.php" method="post" accept-charset="utf-8">
+		  <?php  
+      require "../connect.php";
+			$abc=mysqli_query($conn,"select letter_id from letter where mcms is NULL");
+      if(mysqli_num_rows($abc)>0){
+      $select= '<select name="select">';
+      while($rs=mysqli_fetch_array($abc)){
+     
+      $select.='<option name="lid" value="' .$rs[0]. '">'.$rs[0].'</option>';
+       }
+      }
+      
+      echo $select;
+      echo '</select>'?>
+      <input type="date" name="datemsao" placeholder="Date sent to MS" required>
+      <input type="submit" name="submitt" value="Enter Date">
+      <input type="reset" name="reset" value="Reset">
+	</form>
+</div>
 	</div>
     
        <script>
