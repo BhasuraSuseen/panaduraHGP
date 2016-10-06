0bb20c34574f004471c3e1045d627eff37a1cbf8
ramba93 <munasinghetd@gmail.com>
2016-10-06 23:53:45 +0530

    report updte

--- attendence/viewEbArp.php
+++ attendence/viewEbArp.php
@@ -1,10 +1,10 @@
 
 <?php
 include 'datetime.php';
-include 'connection.php';
+include '../connect.php';
 
 
-$presentqury =mysqli_query($server, "SELECT
+$presentqury =mysqli_query($conn, "SELECT
   COUNT(attend.$day)
 FROM attend
   INNER JOIN employee
