56182c72526df98191312b889925a62a32c46eb8
ramba93 <munasinghetd@gmail.com>
2016-10-06 23:34:48 +0530

    icon ok v1

--- sysadmin.php
+++ sysadmin.php
@@ -9,7 +9,7 @@
 </head>
 <body> 
 	<div class="all_container">
-		<div class="left_side_bar" style="position: relative;top: -80px;">
+		<div class="left_side_bar" style="position: relative;top: -78px;">
             <div class="left_side_bar_top">                   
             </div>
             <div class="left_side_bar_down" >
