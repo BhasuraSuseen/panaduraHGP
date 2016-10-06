0fb7c8e210d92c8301ed2a20f94a436b08152df5
ramba93 <munasinghetd@gmail.com>
2016-10-06 17:32:35 +0530

    day

--- attendence/ebatt.php
+++ attendence/ebatt.php
@@ -28,8 +28,8 @@ FROM employee
                
                 <h5 ><? //php echo $date2  ?></h5>
         <h5 id="date" style="display: none"><?php echo $date3 ?></h5>
-        <h5 style="display: none"><?php echo $date4 ?></h5>
-        <h5 style="diplay: none"><?php echo $day ?></h5>
+        <h5><?php echo $date4 ?></h5>
+        <h5 style="diplay: none"><?php// echo $day ?></h5>
         <body>
             
             
