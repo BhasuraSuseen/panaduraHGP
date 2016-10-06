2ca0c3209da62e02d9b179edd82f3d0b04b9fe15
ramba93 <munasinghetd@gmail.com>
2016-10-06 18:30:00 +0530

    update query

--- attendence/saveattends.php
+++ attendence/saveattends.php
@@ -18,7 +18,7 @@ echo $NIC .$DATE .$Atten.$day.$yearmon;
 
 //mysqli_query($server, "insert into attleave(date,employee_nic,attend) values ('$DATE','$NIC','$val')");
     //mysqli_query($server, "update attend set $day= '$val' WHERE employee_nic='$NIC' and date='$yearmon'");
-   mysqli_query($conn, "update attend set attend.$day = '$val' WHERE employee_nic='$NIC' and date='$yearmon'");
+   mysqli_query($conn, "update attend set `$day` = '$val' WHERE employee_nic='$NIC' and date='$yearmon'");
     echo 'Hri mcn';
 } catch (Exception $exc) {
 }
