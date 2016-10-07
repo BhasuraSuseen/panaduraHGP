a189b94fc5efc4d5f8bb28ee523a549b24a094f5
ramba93 <munasinghetd@gmail.com>
2016-10-08 00:01:17 +0530

    ui ebatt

--- attendence/eb.php
+++ attendence/eb.php
@@ -281,7 +281,7 @@
 
                     <br>
 
-                    <div style="height: 600px">
+                    <div style="height: 600px; width:95%" >
 
                         <div style="overflow-y: scroll">
 
@@ -313,6 +313,7 @@
                             <tr><td style="width:40%">Search by name:</td><td><input type="text" name="sername"></td></tr>
                             <tr><td style="width:40%">Search by ID :</td><td><input type="text" name="serId"></td></tr>
                             <tr><td style="width:40%">Status:</td><td><input type="text" name="status"></td></tr>
+				<button onclick="searchatt()">Search</button>
                         </table>
                     </div>
                     
@@ -324,8 +325,6 @@
             </div>
             <div class="right_side_bar"> 
 
-                <br>
-                <br>
                 <ul id="nav">
                 <button onclick="loadEbdata()"/>
                 
@@ -340,29 +339,9 @@
       <path d="m223.5,335h96c4.143,0 7.5-3.358 7.5-7.5s-3.357-7.5-7.5-7.5h-96c-4.143,0-7.5,3.358-7.5,7.5s3.357,7.5 7.5,7.5z"/>
       <path d="m186.196,306.197l-26.696,26.697-10.696-10.697c-2.93-2.929-7.678-2.929-10.607,0s-2.929,7.678 0,10.606l16,16c1.465,1.464 3.385,2.197 5.304,2.197s3.839-0.732 5.304-2.197l32-32c2.929-2.929 2.929-7.678 0-10.606-2.931-2.929-7.679-2.929-10.609,0z"/>
     
-</svg>
-
-            
-            
+</svg>      
      ...........      
-</button> 
-                
-                
-                <button id="btn1"/>
-                <svg version="1.1" id="Capa_1"  x="0px" y="0px"
-	 viewBox="0 0 250.313 250.313" style="enable-background:new 0 0 250.313 250.313;" xml:space="preserve">
 
-	<path style="fill-rule:evenodd;clip-rule:evenodd;" d="M244.186,214.604l-54.379-54.378c-0.289-0.289-0.628-0.491-0.93-0.76
-		c10.7-16.231,16.945-35.66,16.945-56.554C205.822,46.075,159.747,0,102.911,0S0,46.075,0,102.911
-		c0,56.835,46.074,102.911,102.91,102.911c20.895,0,40.323-6.245,56.554-16.945c0.269,0.301,0.47,0.64,0.759,0.929l54.38,54.38
-		c8.169,8.168,21.413,8.168,29.583,0C252.354,236.017,252.354,222.773,244.186,214.604z M102.911,170.146
-		c-37.134,0-67.236-30.102-67.236-67.235c0-37.134,30.103-67.236,67.236-67.236c37.132,0,67.235,30.103,67.235,67.236
-		C170.146,140.044,140.043,170.146,102.911,170.146z"/>
-
-        </svg>
-            ...........
-            </button> 
-               
                 </ul>
     </body>
     <script type="text/javascript" src="jst.js"></script>
