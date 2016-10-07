02305e422fc46a8ad7c8f8246e7dfd0a841a9235
BhasuraSuseen <bhasuras@live.com>
2016-10-08 01:26:32 +0530

    iframe styles 002

--- sysadmin.php
+++ sysadmin.php
@@ -75,7 +75,7 @@
 		<a href="attendence/eb.php"> Post box and news feed</a><br>
 		<a href="vertical-timeline/timeline.php"> vertical timeline</a>
 		    
-		<iframe src="vertical-timeline/timeline.php" style="float: left; width:100%;height:auto; padding: 5px;"></iframe>
+		<iframe src="vertical-timeline/timeline.php" style="float: left; width:100%;height:100%; padding: 5px; overflow: hidden;"></iframe>
 
             </div>
             <div class="middle_right">
