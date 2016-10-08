c7d84ff5726aad7fd2a8a55bbc260a6adb71a253
ramba93 <munasinghetd@gmail.com>
2016-10-08 15:41:45 +0530

    serch ajx update

--- attendence/jst.js
+++ attendence/jst.js
@@ -170,47 +170,31 @@ function saveAttends(name, date, attend) {
     }
 }
 
-//function chng(){
-//    
-//   document.getElementById('a5').src = "WWW.YIFY-TORRENTS.COM.jpg";
-//    
-//}
-//function chng_out(){
-//    
-//   document.getElementById('a5').src = "mthu.jpg";
-//    
-//}
-//
-//
-//
-//
-//    if (!Modernizr.touch || !Modernizr.inputtypes.date) {
-//        $('input[type=date]')
-//            .attr('type', 'text')
-//            .datepicker({
-//                // Consistent format with the HTML5 picker
-//                dateFormat: 'yy-mm-dd'
-//            });
-//    }
-//
-function searchAttends(day,name,id) {
+function searchAttends(day,id) {
     checkBrowser();
+
     obj.onreadystatechange = function() {
 
-        alert(obj.responseText);
+        //alert(obj.responseText);
         if (obj.readyState === 4 && obj.status === 200) {
 
             var text = obj.responseText;
+//            alert(text);
+
+            var abc = "Present";
+
+            if (text == '0'){
+                abc = "Absent";
+            }
             
-            document.getElementById('st1').innerHTML = JSON.parse(text).a;
-            document.getElementById('st2').innerHTML = JSON.parse(text).b;
-            document.getElementById('st3').innerHTML = JSON.parse(text).c;
+            document.getElementById('st5').innerHTML = abc;
+          
 
         }
     };
-    obj.open("POST", "attendence/search.php", true);
-        obj.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        obj.send("nic=" + id + "&day=" + day + "&name=" + name);
+    obj.open("POST", "search.php", true);
+    obj.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    obj.send("nic=" + id + "&day=" + day);
 
 
 }
