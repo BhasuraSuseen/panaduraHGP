a53ccbe1a914316eb5990c2744e75a13e9d9ba84
BhasuraSuseen <bhasuras@live.com>
2016-10-10 12:21:56 +0530

    remove

--- vertical-timeline/timeline.php
+++ vertical-timeline/timeline.php
@@ -1,21 +1,4 @@
-<!doctype html>
-<html lang="en" class="no-js">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>
-
-	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
-	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
-	<script src="js/modernizr.js"></script> <!-- Modernizr -->
-  	
-	<title>Responsive Vertical Timeline</title>
-</head>
-<body>
-	<header>
-		<h1>Post message here</h1>
-	</header>
 
 	<section id="cd-timeline" class="cd-container" style="height: inherit; overflow-y: scroll;">
 		<div class="cd-timeline-block">
@@ -97,5 +80,4 @@
 	</section> <!-- cd-timeline -->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="js/main.js"></script> <!-- Resource jQuery -->
-</body>
-</html>
+
