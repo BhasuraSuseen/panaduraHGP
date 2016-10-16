53d7cd48f1caacf1030a7200d383ce4c1b77583e
ramba93 <munasinghetd@gmail.com>
2016-10-16 16:26:32 +0530

    back

--- attendence/search.php
+++ attendence/search.php
@@ -1,29 +1,4 @@
 
-                        <table border="0" style="width: 100%">
-                            <tr>Employee came</tr>
-                            <tr><td style="width:40%">Date:</td><td><select style="width:60%" id="day">
-
-
-                                        <?php
-                                        for ($x = 1; $x <= 30; $x++) {
-                                            if ($x < 10) {
-                                                ?>
-                                                <option>0<?php echo $x; ?></option>
-
-                                            <?php } else if ($x >= 10) { ?>
-
-                                                <option><?php echo $x; ?></option>
-
-                                                <?php
-                                            }
-                                        }
-                                        ?>
-
-                                </td></tr>
-                            <tr><td style="width:40%">Search by ID :</td><td><input type="text" id="serId"></td></tr>
-                            <tr><td style="width:40%">Status:</td><td><label id="st5"></label></td></tr>
-                            <tr><td style="width: 60%"<br><br></td><td><button onclick="searchAttends(document.getElementById('day').value,document.getElementById('serId').value)" >Search</button></td></tr>
-                        </table>
 
 <?php
 
