a1802540545785da722d5b82be2fc33483810571
BhasuraSuseen <bhasuras@live.com>
2016-10-25 09:32:01 -0700

    jason database testing

--- attendancemaileb1.php
+++ attendancemaileb1.php
@@ -5,5 +5,16 @@
 </head>
 <body>
 
+<?php
+require "connect.php";
+  $query1 = "SELECT * FROM employee";
+  $result1 = mysqli_query($con,$query);
+  $rows1 = array[];
+  while($r1 = mysqli_fetch_array($result)) {
+    $rows1[] = $r1;
+  }
+  echo json_encode($rows1);
+  print $rows1
+?>			     		
 </body>
 </html>
