bb88dc80f14b3c981a9c457efdc85c07b2a5e703
BhasuraSuseen <bhasuras@live.com>
2016-10-27 18:00:19 -0700

    useless removed

--- sysadmin.php
+++ sysadmin.php
@@ -104,7 +104,7 @@ require "connect.php";
 			   <div class="middle_top_right">
 			   		<?php
 			   		 
-			   		echo"<a>".$_SESSION['username']."</a>";
+			   		echo"<a><spanuser>".$_SESSION['username']."</spanuser></a>";
 			   			 ?>
 				  
 				  <a href="index.php"><button class="button">Log Out</button></a>
@@ -122,14 +122,12 @@ require "connect.php";
 		-->
             </div>
             <div class="middle_right">
-                <button class="button button1">GA</button>
+            
             </div>
 		</div>
 		<div class="right_side_bar" id="open_att_1">
 			
-            <svg xmlns="http://www.w3.org/2000/svg" class="search-panel-query-icon" width="24px" height="24px" viewBox="0 0 24 24"  >
-                <path fill="#455A64" d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-             </svg>
+           
             
         </div>
 	
