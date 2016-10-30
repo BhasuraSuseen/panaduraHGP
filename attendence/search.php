f0c72f37967eeb0250e54586780269d192d0271d
ramba93 <munasinghetd@gmail.com>
2016-10-30 11:40:08 +0530

    Update search.php

--- attendence/search.php
+++ attendence/search.php
@@ -8,10 +8,10 @@ include '../connect.php';
 $NIC = $_POST['nic'];
 $DAY = $_POST['day'];
 $res = mysqli_query($conn, "select attend.employee_nic,
-attend.$DAY,
+attend.30,
 employee.F_name,
 employee.L_name FROM
-employee INNER JOIN attend ON attend.Employee_Nic = employee.Nic_no where attend.employee_nic='$NIC'");
+employee INNER JOIN attend ON attend.Employee_Nic = employee.Nic_no where attend.employee_nic='85112342V'");
 //$res = mysqli_query($server, "select attend.employee_nic,attend.$day,employee.F_name,employee.L_name FROM employee INNER JOIN attend ON attend.Employee_Nic = employee.Nic_no where  ");
 //echo $NIC . $DAY;
 if ($NIC == NULL || $NIC == "") {
