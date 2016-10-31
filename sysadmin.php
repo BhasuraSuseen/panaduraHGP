bcaf76fbd6cd893e9c82f63e24b61677d3b6b5be
BhasuraSuseen <bhasuras@live.com>
2016-10-31 09:27:47 -0700

    php included

--- sysadmin.php
+++ sysadmin.php
@@ -123,9 +123,71 @@ require "connect.php";
 		-->
 		
 		<p> Hellow </p>
+    
     <?php
-        include = "lettercounts.php";
-    ?>
+require "connect.php";
+//mail clerk 1 letter count//
+$mcsql1="select * from letter where date is NOT NULL and mcms IS NULL";
+$a=mysqli_query($conn,$mcsql1);
+$a1=mysqli_num_rows($a);
+//echo $a1;
+//medical spurintendant letter count//
+$mssql1="select * from letter where mcms is NOT NULL and msao IS NULL";
+$b=mysqli_query($conn,$mssql1);
+$b1=mysqli_num_rows($b);
+//echo $b1;
+//admin officer letter count//
+$aosql1="select * from letter where msao is NOT NULL and aomc IS NULL";
+$c=mysqli_query($conn,$aosql1);
+$c1=mysqli_num_rows($c);
+//echo $c1;
+//mail clerk letter count 2//
+$mcsql2="select * from letter where aomc is NOT NULL and mceb IS NULL";
+$d=mysqli_query($conn,$mcsql2);
+$d1=mysqli_num_rows($d);
+//echo $d1;
+//eb1 letter count//
+$eb1sql="select * from letter where mceb is NOT NULL and eb='1'";
+$eb1=mysqli_query($conn,$eb1sql);
+$eb11=mysqli_num_rows($eb1);
+//echo $eb11;
+//eb2 letter count//
+$eb1sq2="select * from letter where mceb is NOT NULL and eb='2'";
+$eb2=mysqli_query($conn,$eb2sql);
+$eb22=mysqli_num_rows($eb2);
+//echo $eb22;
+//eb3 letter count//
+$eb3sql="select * from letter where mceb is NOT NULL and eb='3'";
+$eb3=mysqli_query($conn,$eb3sql);
+$eb33=mysqli_num_rows($eb3);
+//echo $eb33;
+//eb4 letter count//
+$eb4sql="select * from letter where mceb is NOT NULL and eb='4'";
+$eb4=mysqli_query($conn,$eb4sql);
+$eb44=mysqli_num_rows($eb4);
+//echo $eb44;
+//eb5 letter count//
+$eb5sql="select * from letter where mceb isNOT NULL and eb='5'";
+$eb5=mysqli_query($conn,$eb5sql);
+$eb55=mysqli_num_rows($eb5);
+//echo $eb55;
+//eb6 letter count//
+$eb6sql="select * from letter where mceb is NOT NULL and eb='6'";
+$eb6=mysqli_query($conn,$eb6sql);
+$eb66=mysqli_num_rows($eb6);
+//echo $eb66;
+//eb7 letter count//
+$eb7sql="select * from letter where mceb is NOT NULL and eb='7'";
+$eb7=mysqli_query($conn,$eb7sql);
+$eb77=mysqli_num_rows($eb7);
+//echo $eb77;
+//eb8 letter count//
+$eb8sql="select * from letter where mceb is NOT NULL and eb='8'";
+$eb8=mysqli_query($conn,$eb8sql);
+$eb88=mysqli_num_rows($eb8);
+//echo $eb88;
+?>   
+    
     <script>
       d3.select("p").text("D3 is working");
       var width = 600;
