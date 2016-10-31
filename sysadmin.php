b08f20cd620487a7b494a84d853fd73843a7444d
BhasuraSuseen <bhasuras@live.com>
2016-10-31 04:07:30 -0700

    script

--- sysadmin.php
+++ sysadmin.php
@@ -8,7 +8,8 @@
 	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui"/>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	<script type="text/javascript" src="attendence/jst.js"></script>
-		<script type="text/javascript">
+	<script  src="http://d3js.org/d3.v3.min.js"></script>
+	<script type="text/javascript">
 
   function checkForm(form)
   {
