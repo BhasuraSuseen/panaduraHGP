f094131cfa5f66f528d8ae2ae090716cca3808ec
RidmiShanika <ridmishanika1992@gmail.com>
2016-10-31 15:59:46 +0530

    Update dietui.php

--- Diet/dietui.php
+++ Diet/dietui.php
@@ -10,58 +10,7 @@
         <script type="text/javascript" src="attendence/jst.js"></script>
         <script type="text/javascript">
 
-            function checkForm(form)
-            {
-                if (form.user_name.value == "") {
-                    alert("Error: Username cannot be blank!");
-                    form.user_name.focus();
-                    return false;
-                }
-                re = /^\w+$/;
-                if (!re.test(form.user_name.value)) {
-                    alert("Error: Username must contain only letters, numbers and underscores!");
-                    form.user_name.focus();
-                    return false;
-                }
-
-                if (form.password.value != "" && form.password.value == form.passwordc.value) {
-                    if (form.password.value.length < 6) {
-                        alert("Error: Password must contain at least six characters!");
-                        form.password.focus();
-                        return false;
-                    }
-                    if (form.password.value == form.user_name.value) {
-                        alert("Error: Password must be different from Username!");
-                        form.password.focus();
-                        return false;
-                    }
-                    re = /[0-9]/;
-                    if (!re.test(form.password.value)) {
-                        alert("Error: password must contain at least one number (0-9)!");
-                        form.password.focus();
-                        return false;
-                    }
-                    re = /[a-z]/;
-                    if (!re.test(form.password.value)) {
-                        alert("Error: password must contain at least one lowercase letter (a-z)!");
-                        form.password.focus();
-                        return false;
-                    }
-                    re = /[A-Z]/;
-                    if (!re.test(form.password.value)) {
-                        alert("Error: password must contain at least one uppercase letter (A-Z)!");
-                        form.password.focus();
-                        return false;
-                    }
-                } else {
-                    alert("Error: Please check that you've entered and confirmed your password!");
-                    form.password.focus();
-                    return false;
-                }
-
-                alert(form.user_name.value + " was entered successfully");
-                return true;
-            }
+            
 
         </script>
     </head>
