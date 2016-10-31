dba1959cd6705348559ae46b3826eed6ccf29afc
BhasuraSuseen <bhasuras@live.com>
2016-10-31 03:12:47 -0700

    database

--- connect.php
+++ connect.php
@@ -1,11 +1,21 @@
 
 <?php
-define('BASE_PATH','http://localhost/panaduraHGP/');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'homs1');
 define('DB_USER','root');
 define('DB_PASSWORD','');
 
-$conn=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to database: " . mysql_error());
-$db=mysqli_select_db($conn,DB_NAME) or die("Failed to connect to database: " . mysqli_error());
+define('DB_HOST1', 'us-cdbr-azure-southcentral-f.cloudapp.net');
+define('DB_NAME1', 'homs1');
+define('DB_USER1','b7f626611d6e2d');
+define('DB_PASSWORD1','09f1186c');
+
+
+if ($_SERVER['HTTP_HOST'] == 'localhost'){
+    $conn=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to database: " . mysql_error());
+    $db=mysqli_select_db($conn,DB_NAME) or die("Failed to connect to database: " . mysqli_error());
+}else{
+    $conn=mysqli_connect(DB_HOST1,DB_USER1,DB_PASSWORD1) or die("Failed to connect to database: " . mysql_error());
+    $db=mysqli_select_db($conn,DB_NAME1) or die("Failed to connect to database: " . mysqli_error());
+}
 ?>
