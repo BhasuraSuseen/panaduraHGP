2f24de9a9ff658efb480f357bb85f8a442d42091
RidmiShanika <ridmishanika1992@gmail.com>
2016-11-08 18:56:19 +0530

    Update menu_view.php

--- Diet/menu_view.php
+++ Diet/menu_view.php
@@ -1,24 +1,26 @@
-<html>
-    <body>
-        
+
+
         <?php
         //if(isset($_POST['submit'])){
         $day = $_POST['day'];
         $type = $_POST['type'];
 
-        if($day == "tuesday") {
-        include 'menu_tue.php';
-        }else if($day == "monday"){
-			include 'menu_mon.php';
-		} else if($day == "wednesday"){
-			include 'menu_wed.php';
-		}else if($day == "thursday"){
-			include 'menu_thu.php';
-		}else if($day == "friday"){
-			include 'menu_fri.php';
-		}
+        if ($day == "Tuesday") {
+            include 'menu_tue.php';
+        } else if ($day == "Monday") {
+            include 'menu_mon.php';
+        } else if ($day == "Wednesday") {
+            include 'menu_wed.php';
+        } else if ($day == "Thursday") {
+            include 'menu_thu.php';
+        } else if ($day == "Friday") {
+            include 'menu_fri.php';
+        }
+        
+        
+        
+        ?>
 
-         ?>
-    </body>
-</html>
+
+
 
