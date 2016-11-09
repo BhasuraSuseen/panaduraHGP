657d1376f1d20139c4a84af9bb5105e9e5037f42
ramba93 <munasinghetd@gmail.com>
2016-11-09 11:38:03 +0530

    report

---
+++ attendence/report.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'datetime.php';
+include 'connection.php';
+?>
+
+
+<?php
+
+echo 'Panadura base hospital';
+echo "<br>";
+echo 'Month attendence report                                                                                                   ';
+echo $yearmon;
+
+require 'pdfcrowd.php';
+
+try
+{   
+    // create an API client instance
+    $client = new Pdfcrowd("dhananjaya", "be4790e49b23fdf1e17b2317758d46e3");
+
+    // convert a web page and store the generated PDF into a $pdf variable
+    $pdf = $client->convertURI('http://omspanadura.azurewebsites.net/sysadmin.php/');
+
+    // set HTTP response headers
+    header("Content-Type: application/pdf");
+    header("Cache-Control: max-age=0");
+    header("Accept-Ranges: none");
+    header("Content-Disposition: attachment; filename=\"google_com.pdf\"");
+
+    // send the generated PDF 
+    echo $pdf;
+}
+catch(PdfcrowdException $why)
+{
+    echo "Pdfcrowd Error: " . $why;
+}
+?>
+    
