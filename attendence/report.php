20bcb7b971d4d4196be180523ace3d8131c8b08b
ramba93 <munasinghetd@gmail.com>
2016-11-09 11:50:57 +0530

    pdf

--- attendence/report.php
+++ attendence/report.php
@@ -13,6 +13,45 @@ echo 'Month attendence report                                                   
 echo $yearmon;
 
 require 'pdfcrowd.php';
+?>
+<html>
+ <table border="1" style="width: 100%">
+                <tr>
+                    <td style="background-color: appworkspace">ID</td>
+                    <td style="background-color: appworkspace">Name</td>
+                    <td style="width: 8%; background-color: blanchedalmond">present</td>
+                    <td style="width: 8%; background-color: appworkspace">absent</td>
+                    
+
+                </tr>
+
+                <tr>
+<?php while ($row = mysqli_fetch_array($res)):; ?>
+    <?php if ($row['E_type'] == "para") { ?>
+                            <td style="width: 15%; "><input type="text" value="<?php echo $row['Nic_no']; ?>"  id="NIC" readonly></td>
+                            <td style=""><input type="text" value="<?php echo $row['F_Name'] . " " . $row['L_Name']; ?>" name="name"</td>
+
+        <?php } ?>
+
+
+
+                        </tr>
+    
+<?php endwhile; ?>
+
+            </table>
+
+            <!--<button onclick="loadParadata()">aaaa</button>-->
+
+
+    </center>
+</body>
+
+<script type="text/javascript" src="jst.js"></script>
+
+</html>
+
+<?php
 
 try
 {   
