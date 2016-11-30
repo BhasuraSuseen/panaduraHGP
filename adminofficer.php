15f3ea01e03c1fdae4cd27dc3040d33fb1a80979
BhasuraSuseen <bhasuras@live.com>
2016-11-29 23:19:37 -0800

    min-height: 100px;

--- adminofficer.php
+++ adminofficer.php
@@ -188,8 +188,8 @@ $.ajax({
 			
           <div id="middle_leftp1">
   <div class="current-flow-left" style="float: left; width:65%;">
-  <div class="search-letters" style="width:100%; margin: 10px;  background: #fff;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-    <div style="width: 100%; background-color: #2980b9;"><a style="min-height: 50px; font-size: 16px; color: #fff; padding: 10px;">SEARCH LETTER</a></div>
+  <div class="search-letters" style="width:100%; min-height: 100px; margin: 10px;  background: #fff;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+    <div style="width: 100%; background-color: #2980b9;"><a style=" font-size: 16px; color: #fff; padding: 10px;">SEARCH LETTER</a></div>
 	
       <input type="text" id="search-box"  placeholder=" Select by letter ID" style="float: left;width:30%; margin: 10px; background:#fff;"/>
       <input type="text" id="search-box1" placeholder=" Select by Subject" style="float: left;width:30%; margin: 10px;background:#fff;"/>
