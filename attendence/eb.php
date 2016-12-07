f69bfe06ed43e7a6792898266d6ac54c05090ffc
RidmiShanika <ridmishanika1992@gmail.com>
2016-12-07 11:31:55 +0530

    test

--- attendence/eb.php
+++ attendence/eb.php
@@ -14,15 +14,17 @@
             <div class="container">
                 <div class="middle_left">
                     <div style="height: 650px">
-                       Tis is test...
+                
                             <?php include 'ebatt.php'; ?>
                        
                     </div>
                 </div>
                 <div class="middle_right">
                     <!--<button class="button button1">GA</button>-->
-                    <div class="recordstyl"> 
+                    <div class="recordstyl">
+                               This is test...
                         <table border="0" style="width: 100%">
+                            
                             <tr><td style="width:70%">Employees Present :</td><td><strong id="st1"></strong></td></tr>
                             <tr><td style="width:70%">Employees absent :</td><td><strong id="st2"></strong></td></tr>
                             <tr><td style="width:70%">Total Employees :</td><td><strong id="st3"></strong></td></tr>
