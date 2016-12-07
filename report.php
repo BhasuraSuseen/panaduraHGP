01212dc9510899287dece04d1f4f4b040f55cf9b
RidmiShanika <ridmishanika1992@gmail.com>
2016-12-07 22:35:30 +0530

    Update report.php

--- report.php
+++ report.php
@@ -1,10 +1,15 @@
 <?php 
 include './datetime.php';
 include './connect.php';
-$a1=mysqli_query($conn,"SELECT item.Item_name,unit1, report.total  FROM item INNER JOIN report ON item.Item_id = report.Item_id  WHERE total <> 0");?>
+$a1=mysqli_query($conn,"SELECT item.Item_name,unit1, report.total,lastenter FROM item INNER JOIN report ON item.Item_id = report.Item_id  WHERE total <> 0");
+$b1=mysqli_query($conn,"SELECT lastenter FROM report");
+$b = mysqli_fetch_array($b1)
+
+
+?>
 	
 	<center>
-	
+	<h3>Monday Breakfast to <?php echo $b[0];?> Diet Report</h3>
 	<table align="center" style="background-color: #CFD8DC;width: 45%"">
 					<tr>
                         <td align="left"><b>Item Name</b></td>
@@ -16,11 +21,15 @@ $a1=mysqli_query($conn,"SELECT item.Item_name,unit1, report.total  FROM item INN
 					<tr>
                         <td align="left"><?php echo $a[0]; ?></td>
                         <td align="left"><?php echo $a[2].' '.$a[1]; ?></td></tr>
+						 
 				<?php 
 				
-		 endwhile; ?>
+		 endwhile; 
+		
+		 ?>
 			</table>
 			</center>
+
 
 
 
