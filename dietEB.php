d1a47bac150ca40741b48a2a156ab21fad24a406
Himashini <whima115@yahoo.com>
2017-01-09 19:12:36 +0530

    Update dietEB.php

--- dietEB.php
+++ dietEB.php
@@ -226,7 +226,7 @@
 	
 	<div id="pop_box_hr_3">
 	<div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;"> LETTER UPDATE </a></div>
-                <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">	
+        <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">	
 	 <form name="mctoeb" action="dietEB.php" method="post" accept-charset="utf-8">
     <b>Enter Letter EB to EB date</b><br>
     <label>Letter ID</label>
@@ -263,7 +263,7 @@
     ?>
 
 		
-                   
+		</div>           
  	</div>
 
 	</div>
