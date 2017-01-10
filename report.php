13d02017e447ac7ec2618d83b8d723c82ff89985
Himashini <whima115@yahoo.com>
2017-01-10 20:51:23 +0530

    Update report.php

--- report.php
+++ report.php
@@ -12,15 +12,15 @@ $b = mysqli_fetch_array($b1)
 	Panadura Base Hospital-Weekly Diet Report<br><?php echo "Year: " .  date("Y");?><br><?php echo "Month: " .  date("M");?><br><?php echo "Week No: " .  date("W");?><br>Period: Monday Breakfast to <?php echo $b[0];?><br>
 	<center><table align="center" style="background-color: #CFD8DC;width: 45%"">
 					<tr>
-                        <td align="left"<b>Item Name</b></td>
-                        <td align="left"><b>Amount</b><br></td>
+                        <td align=left><b>Item Name</b></td>
+                        <td align=left><b>Amount</b><br></td>
                     </tr><br>
 		 <?php while ($a = mysqli_fetch_array($a1)):; ?>
 				
  
 					<tr>
-                        <td align="left"><?php echo $a[0]; ?></td>
-                        <td align="left"><?php echo $a[2].' '.$a[1]; ?></td></tr>
+                        <td align=left><?php echo $a[0]; ?></td>
+                        <td align=left><?php echo $a[2].' '.$a[1]; ?></td></tr>
 						 
 				<?php 
 				
