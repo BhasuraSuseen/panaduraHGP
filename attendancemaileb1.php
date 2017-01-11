66ec97596bffeca030ad9067604226bef6609632
Bhasura Suseen Gunawardhana <bhasuras@live.com>
2017-01-10 23:30:51 -0800

    EB1 pop box complete

--- attendancemaileb1.php
+++ attendancemaileb1.php
@@ -37,14 +37,14 @@
                  <li id="open_mail_1" title="Update Letter Details" style="border-left: 4px solid #c62828;">  
 						<i> <img src="images/web.png" style="width:80%;height:auto; opacity: 0.6; padding: 5px;"></i>
 					</li>
-           <li  id="open_mail_1" title="Reply to Letters"  class="letter_panel_list">  
+           <li  id="open_mail_2" title="Reply to Letters"  class="letter_panel_list">  
                   <i> <img src="images/mailrep.ico" style="width:80%;height:auto; opacity: 0.6; padding: 5px;">
                   </i>
                   </li>
-				 <li id="open_att_1" style="border-left: 4px solid #2980b9;">  
+				 <li id="open_hr_1" style="border-left: 4px solid #2980b9;">  
 						<i> <img src="images/check.png" style="width:80%;height:auto; opacity: 0.6; padding: 5px;"></i>
 					</li>
-          <li id="open_att_1" style="border-left: 4px solid #2980b9;">  
+          <li id="open_hr_2" style="border-left: 4px solid #2980b9;">  
             <i> <img src="images/leave.png" style="width:80%;height:auto; opacity: 0.6; padding: 5px;"></i>
           </li>
               </ul>
@@ -84,7 +84,7 @@
           </div>
           <div class="right_side_bar_down">
               <ul id="nav">
-                 <li id="open_hr_1" style="border-right: 4px solid #2980b9;"> 
+                 <li id="open_report_1" style="border-right: 4px solid #2980b9;"> 
 
 						<i> <img src="images/businessman-7.png" style="width:80%;height:auto; opacity: 0.6; padding: 5px;"></i>
 					</li>
@@ -92,63 +92,116 @@
               </ul>
           </div> 
           	</div>
+     </div>
 		 <div id="pop_background"></div>
-            <div id="pop_box_hr_1">
-			<form name="mctoeb" action="attendancemaileb1.php" method="post" accept-charset="utf-8">
-    <b>Enter Letter EB to EB date</b><br>
-  <label>Letter ID</label>
-      <?php  
-      $eb=$_SESSION['ebno'];
-      $abcd=mysqli_query($conn,"select letter_id from letter where mceb is NULL and eb ='".$eb."'");
-      if(mysqli_num_rows($abcd)>0){
-      $select= '<select name="lid">';
-      while($rsl=mysqli_fetch_array($abcd)){
-     
-      $select.='<option value="' .$rsl[0]. '">'.$rsl[0].'</option>';
-       }
-      }
-       else{
-        $select='<select><option></option></select>';
-      }
-      echo $select;
-      echo '</select>'?>
-      <input type="date" name="datemceb" placeholder="Date recieved from Letter EB" required>
-      <input type="submit" name="submitt" value="Enter Date">
-      <input type="reset" name="reset" value="Reset">
-  </form>
-  <?php
-   if(isset($_POST["submitt"])){
-    if(isset($_POST['lid'])){
+            <div id="pop_box_mail_1">
+            <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">LETTER UPDATE </a></div>
+                  <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+                			<form name="mctoeb" action="attendancemaileb1.php" method="post" accept-charset="utf-8">
+                    <b>Enter Letter EB to EB date</b><br>
+                  <label>Letter ID</label>
+                      <?php  
+                      $eb=$_SESSION['ebno'];
+                      $abcd=mysqli_query($conn,"select letter_id from letter where mceb is NULL and eb ='".$eb."'");
+                      if(mysqli_num_rows($abcd)>0){
+                      $select= '<select name="lid">';
+                      while($rsl=mysqli_fetch_array($abcd)){
+                     
+                      $select.='<option value="' .$rsl[0]. '">'.$rsl[0].'</option>';
+                       }
+                      }
+                       else{
+                        $select='<select><option></option></select>';
+                      }
+                      echo $select;
+                      echo '</select>'?>
+                      <input type="date" name="datemceb" placeholder="Date recieved from Letter EB" required>
+                      <input type="submit" name="submitt" value="Enter Date">
+                      <input type="reset" name="reset" value="Reset">
+                  </form>
+                  <?php
+                   if(isset($_POST["submitt"])){
+                    if(isset($_POST['lid'])){
 
-$lid=$_POST['lid'];
-$datemceb=$_POST['datemceb'];
+                $lid=$_POST['lid'];
+                $datemceb=$_POST['datemceb'];
 
-$sql="UPDATE letter SET mceb='$datemceb' WHERE letter_id='$lid'";
-mysqli_query($conn,$sql);
-$message= $lid. " was updated";
-  echo  "<script type='text/javascript'>alert('$message');</script>";
-}
-else{
-  $message="No letters to reply";
-  echo  "<script type='text/javascript'>alert('$message');</script>";
-  
-}
+                $sql="UPDATE letter SET mceb='$datemceb' WHERE letter_id='$lid'";
+                mysqli_query($conn,$sql);
+                $message= $lid. " was updated";
+                  echo  "<script type='text/javascript'>alert('$message');</script>";
+                }
+                else{
+                  $message="No letters to reply";
+                  echo  "<script type='text/javascript'>alert('$message');</script>";
+                  
+                }
 
-}
-?>
+                }
+                ?>
+                </div>
             </div>
-	</div>
+            <div id="pop_box_mail_2">
+                <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">LETTER REPLY FORM </a></div>
+                          <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
+                                    <?php echo "include here"; ?> 
+                          </div>
+            </div>
+            <div id="pop_box_hr_1">
+                  <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">ATTENDANCE </a></div>
+                  <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
+                                    <?php echo "include here"; ?> 
+                  </div>
+            </div>
+            <div id="pop_box_hr_2">
+                  <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">LEAVE </a></div>
+                  <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
+                                    <?php echo "include here"; ?> 
+                  </div>
+            </div>
+            <div id="pop_box_report_1">
+                  <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">EMPLOYEE DETAILS </a></div>
+                  <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
+                                    <?php echo "include here"; ?> 
+                  </div>
+            </div>
+	
     
         <script>
          $(document).ready(function(){
             $('#open_mail_1').click(function(){
                 $('#pop_background').fadeIn();
+                $('#pop_box_mail_1').fadeIn();
+                return false;
+            });
+            $('#open_mail_2').click(function(){
+                $('#pop_background').fadeIn();
+                $('#pop_box_mail_2').fadeIn();
+                return false;
+            });
+            $('#open_hr_1').click(function(){
+                $('#pop_background').fadeIn();
                 $('#pop_box_hr_1').fadeIn();
                 return false;
             });
+            $('#open_hr_2').click(function(){
+                $('#pop_background').fadeIn();
+                $('#pop_box_hr_2').fadeIn();
+                return false;
+            });
+            $('#open_report_1').click(function(){
+                $('#pop_background').fadeIn();
+                $('#pop_box_report_1').fadeIn();
+                return false;
+            });
+
             $('#pop_background').click(function(){
                 $('#pop_background').fadeOut();
+                $('#pop_box_mail_1').fadeOut();
+                $('#pop_box_mail_2').fadeOut();
                 $('#pop_box_hr_1').fadeOut();
+                $('#pop_box_hr_2').fadeOut();
+                $('#pop_box_report_1').fadeOut();
  
                 return false;
             });
