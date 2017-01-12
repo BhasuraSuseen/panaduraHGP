1cd3327ec8251540e7e3c62c1915cfe3a70951d2
Himashini <whima115@yahoo.com>
2017-01-12 23:27:59 +0530

    Update dietEB.php

--- dietEB.php
+++ dietEB.php
@@ -190,7 +190,8 @@
 		<div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;"> DIET SCHEDULE </a></div>
 		
 	  		<?php //include 'dui.php';?>
-		<div class="middle_left" style="width:50%; background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+		<div class="middle_left" >
+		<div style="width:100%; background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 			<center><form name="new_count" method="post" action="dietEB.php" onsubmit="return validateForm()">
                             <table>
 				    <thead><tr><th colspan=2 size="+2" color=#2980b9><b>ENTER COUNTS</b></th></tr></thead>
@@ -235,7 +236,8 @@
                                 color: white;" onclick="count(document.getElementsByName('meal'), document.getElementById('s1').value, document.getElementById('s2').value, document.getElementById('s3').value, document.getElementById('patients').value, document.getElementById('dp').value, document.getElementById('staff').value)">Calculate</button>
                     </center>
                 </div>
-		<div class="middle_right" style="width:50%; background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+		<div class="middle_right" >
+		<div style="width:100%; background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 			<center><label style="background-color: #CFD8DC" align="center" id="lb1" ></label></center><br>
 		</div>
 		</div>
