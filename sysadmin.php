7b464d939917fafb5cddeebe6f681d449b564c54
ramba93 <munasinghetd@gmail.com>
2017-01-12 14:54:46 +0530

    pop

--- sysadmin.php
+++ sysadmin.php
@@ -428,7 +428,7 @@ require "connect.php";
       
         <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;"> RECORD LEAVE</a></div>
         <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
-        <?php echo "include here"; ?> 
+        <?php include 'leave/ebLeave' ?> 
         </div>    
       </div>
   
