95643c6928f36692b8a19e4e5c5966dbb81be60c
Himashini <whima115@yahoo.com>
2017-01-12 23:12:56 +0530

    Update dietEB.php

--- dietEB.php
+++ dietEB.php
@@ -188,9 +188,9 @@
 	
      	<div id="pop_box_att_1">
 		<div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;"> DIET SCHEDULE </a></div>
-		<div style="position: absolute; width:100%; background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" >
+		
 	  		<?php //include 'dui.php';?>
-		<div class="middle_left">
+		<div class="middle_left" style="width:100%; background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
 			<center><form name="new_count" method="post" action="dietEB.php" onsubmit="return validateForm()">
                             <table>
 				    <thead><tr><th colspan=2 size="+2" color=#2980b9><b>ENTER COUNTS</b></th></tr></thead>
@@ -241,7 +241,7 @@
 			<center><label style="background-color: #CFD8DC" align="center" id="lb1" ></label></center><br>
 		</div>
 		</div>
-	</div>
+
 	       
 	
 	<div id="pop_box_hr_3">
@@ -286,7 +286,7 @@
 		</div>           
  	</div>
 
-	</div>
+	
 	
   <div>
    
