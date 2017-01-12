8b8882c6e0ecd25944d7c002e313f6346f4e25e7
ramba93 <munasinghetd@gmail.com>
2017-01-12 14:38:32 +0530

    chk

--- attendence/doctor.php
+++ attendence/doctor.php
@@ -62,7 +62,7 @@
 
                 </div>
             </div>
- <script type="text/javascript" src="jst.js"></script>
+ <!--<script type="text/javascript" src="jst.js"></script>-->
             <script>
                 function loadEbdata() {
     checkBrowser();
