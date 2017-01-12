6d67f3d6f60093b3240b6842b19d118e43d27760
Himashini <whima115@yahoo.com>
2017-01-12 18:36:08 +0530

    Update downloadFile.php

--- downloadFile.php
+++ downloadFile.php
@@ -1,6 +1,6 @@
 <?php
-require('fpdf.php');
-class PDF_reciept extends FPDF { 
+require('fpdf/fpdf.php');
+class PDF_report extends FPDF { 
     function __construct ($orientation = 'P', $unit = 'pt', $format = 'Letter', $margin = 40) { 
         $this->FPDF($orientation, $unit, $format); 
         $this->SetTopMargin($margin); 
@@ -14,28 +14,17 @@ class PDF_reciept extends FPDF {
 		$this->SetTextColor(225); 
 		$this->Cell(0, 30, "Panadura Base Hospital-Weekly Diet Report", 0, 1, 'C', true); 
 	}
-	function Footer() { 
-		$this->SetFont('Arial', '', 12); 
-		$this->SetTextColor(0); 
-		$this->SetXY(0,-60); 
-		$this->Cell(0, 20, "Thankyou", 'T', 0, 'C'); 
-	}
+	
 
 }
 $pdf = new PDF_receipt(); 
 $pdf->AddPage(); 
-$pdf->SetFont('Arial', 'B', 12);
+$pdf->SetFont('Arial', '', 12);
 $pdf->SetY(100);
-$pdf->SetX(100);
-$pdf->SetTextColor(0); 
-$pdf->SetFillColor(36, 140, 129);
-$pdf->SetLineWidth(1); 
-$pdf->Cell(427, 25, "Item Name", 'LTR', 0, 'C', true); 
-$pdf->Cell(100, 25, "Amount", 'LTR', 1, 'C', true);
-$y_axis = $y_axis + $rowHeight; 
-$i=0;
-$max=25;
-$rowHeight=6; 
+$pdf->SetFont('Arial', 'B', 12); 
+$pdf->Cell(50, 13, 'Date'); 
+$pdf->SetFont('Arial', ''); 
+$pdf->Cell(100, 13, date('F j, Y'), 0, 1);
 
 
 include './datetime.php';
@@ -46,18 +35,11 @@ $b = mysqli_fetch_array($b1)
 
 
 	
+ 
 while ($a = mysqli_fetch_array($a1)){
-	$item=$a[0];
-	$amount=$a[2].' '.$a[1];
-	
-	$pdf->SetY(100);
-    $pdf->SetX(100);
-    $pdf->Cell(427, 25,$item, 'LTR', 0, 'C', true); 
-	$pdf->Cell(100, 25,$amount, 'LTR', 1, 'C', true);
-
-    //Go to next row
-    $y_axis = $y_axis + $rowHeight;
-    $i = $i + 1;
+	$pdf->Cell(427, 20, echo $a[0] , 1, 0, 'L', $fill); 
+    	$pdf->Cell(100, 20, echo $a[2].' '.$a[1] , 1, 1, 'R', $fill); 
+    	$fill = !$fill;
 }
 $pdf->Output();
 ?>
