23b84f911d85ed20a4bc4b5a6271b0080d1c68e3
ramba93 <munasinghetd@gmail.com>
2017-01-13 14:27:58 +0530

    chng

--- leave/doctorLeave.php
+++ leave/doctorLeave.php
@@ -47,18 +47,18 @@ FROM employee
 
 
 
-                                <td style="width: 12%; background-color: lightgray"><select name="LvType" id="lvt" class="styled-select">
+                                <td style="width: 12%; background-color: lightgray"><select name="LvType" id="lvt" class="attstyled-select">
                                         <option>Mediacal Leave</option>
                                         <option>Half Day</option>
                                         <option>Short Leave</option>
                                         <option>Other</option>
                                     </select></td>
 
-                                <td style="width: 12%; background-color:#ECEFF1 " id="des" >
+                                <td style="width: 12%; background-color:#ECEFF1 " id="des1" >
                                     <input type="text" id="des" name="des">
                                 </td>
 
-                                <td style="width: 12%; background-color:#ECEFF1"><select class="styled-select" name="ReqType" onchange="saveLeave(document.getElementById('NIC').value, document.getElementById('lvt').value, document.getElementById('des').value, this.value, '<?php echo $row[0]; ?>')">
+                                <td style="width: 12%; background-color:#ECEFF1"><select class="attstyled-select" name="ReqType" onchange="saveLeave( '<?php echo $row[0]; ?>', document.getElementById('lvt').value, document.getElementById('des').value, this.value)">
                                         <option>--Select Type--</option>
                                         <option>Letter</option>
                                         <option>Fax</option>
