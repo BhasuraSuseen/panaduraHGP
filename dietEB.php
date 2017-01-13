e6dc3fb8574ebcf4b60052c84bad3ec8f0402dd7
Himashini <whima115@yahoo.com>
2017-01-13 12:25:56 +0530

    Update dietEB.php

--- dietEB.php
+++ dietEB.php
@@ -7,9 +7,18 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	<script type="text/javascript" src="attendence/jst.js"></script>
 	<script  src="http://d3js.org/d3.v3.min.js"></script>
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
-  <script src="scripts/prio2script.js"></script>
-	
+	<SCRIPT language=Javascript>
+      <!--
+      function isNumberKey(evt)
+      {
+         var charCode = (evt.which) ? evt.which : event.keyCode
+         if (charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+
+         return true;
+      }
+      //-->
+   </SCRIPT>
 <style>
     body {
     background-color: #ECEFF1;
@@ -17,37 +26,6 @@
     color: #607D8B ;
     font-weight: 600; 
     }
-    .node{
-      fill:ECEFF1;
-    }
-    .link{
-      stroke: #90A4AE;
-      stroke-width: 10px;
-    }
-    .arc {
-    fill: #ef5350;
-  }
-   .arcb {
-    fill: #CFD8DC;
-  }
-  table {
-    border-collapse: collapse;
-    width: 100%;
-  }
-
-  td, th {
-      text-align: left;
-      padding: 5px;
-      font-weight: 600;
-  }
-
-  tr:nth-child(even) {
-      background-color: #F5F5F5;
-  }
-  #suggesstion-box{ height: 100px; overflow-y: auto; overflow-x: hidden;display: none;}
-  #letterid-list{list-style:none;margin:10px;padding:0;width:100%; }
-  #letterid-list li{padding: 5px; background:#fff;border-bottom:#F0F0F0 1px solid;}
-  #letterid-list li:hover{background:#fafafa;}
    </style>
 
 </head>
@@ -101,42 +79,11 @@
 		    </div>
 			
 			
-            <div id="middle_leftp1">
-                <div class="current-flow-left" style="float: left; width:75%;">
-                <div class="current-flow-left-top" style="float: left; width:100%;">
-                <div class="current-flow-left-top-left" style="width:100%; float: left; ">
-                  <div class="search-letters" style="width:100%; min-height: 80px; margin: 10px; margin-left: 20px;  background: #fff;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                    <div style="width: 100%; float: left;background-color: #2980b9;"><a style=" font-size: 16px; color: #fff; padding: 10px;">SEARCH LETTER</a></div>
-                  
-                      <input type="text" id="search-box"  placeholder=" Select by letter ID" style="float: left;width:30%; margin: 10px; background:#fff;"/>
-                      <input type="text" id="search-box1" placeholder=" Select by Subject" style="float: left;width:30%; margin: 10px;background:#fff;"/>
-                      <input type="text" id="search-box2" placeholder=" Select by Address" style="float: left;width:30%; margin: 10px;background:#fff;" />
-
-                    <div id="suggesstion-box" style="width: 100%; "></div>
-                  
-                </div>
-                </div>
+            <div id="middle_left" >
+		 
                
-              </div>
-                <div class="current-flow" style="float: left; width:100%; margin:10px; margin-left: 20px; margin-top:20px;background: #fff;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                  <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">LETTER DETAILS</a></div>
-                  <div id ="lettercontent" style="width:100%; padding: 20px; overflow:auto;"><p>Hellow</p></div>
-                </div>
-              </div>
-              <script> 
-               func1(1000); 
-               func2(1000);          
-              </script>
-              <div class="current-flow-right" style="float: left; width:20%; margin-left: 20px">
-                <div class="letters-details" style="width:100%; margin: 10px; margin-left: 20px;  background: #fff; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                  <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fff; padding: 10px; ">DISTRIBUTION</a></div>
-                  
-                  <div id ="svgcontent" style="width:100%; padding :10px; overflow:auto; font-size: 14px;"></div>
-                </div>
-              </div>          
-                                                          
-                      
-          </div>
+
+            </div>
 	
             <div class="middle_right">
             
@@ -160,18 +107,18 @@
           </div> 
             </div>
 	
-	</div>
+	
          <div id="pop_background"></div>
 		
-    <div id="pop_box_report_2">
+     		<div id="pop_box_report_2">
 		<div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;"> DIET REPORT </a></div>
 		<div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" >
 	
 				 
                
                     
-    <center><label  style="width: 100%" id="lbpop1"></label><br></center>
-		<?php include 'report.php';?>
+                            <center><label  style="width: 100%" id="lbpop1"></label><br></center>
+				 <?php include 'report.php';?>
 		
 		<center><form action="reportPDF.php" method="post">
 		<button style="background-color: #263238;
@@ -191,7 +138,7 @@
 				
 					
 
-     			
+     			</div>
 	<div id="pop_box_hr_2">
 	<div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;"> SELECT MENU </a></div>	
 	<div style="width:100%;height:70%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">	
@@ -239,14 +186,14 @@
  	</div>
 	</div>
 	
-  <div id="pop_box_att_1" >
+     	<div id="pop_box_att_1">
 	<div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;"> DIET SCHEDULE </a></div>
 	<div style="position: absolute; width:100%; background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">	
 		
 	  		<?php //include 'dui.php';?>
-		<div class="middle_left" style = "width: 50%;">
+		<div class="middle_left" >
 		
-			<center><form name="new_count" method="post" action="dietEB.php">
+			<center><form name="new_count" method="post" action="dietEB.php" onsubmit="return validateForm()">
                             <table>
 				    <thead><tr><th colspan=2 size="+2" color=#2980b9><b>ENTER COUNTS</b></th></tr></thead>
 
@@ -288,10 +235,10 @@
                                 padding: 0.4em;
                                 margin-bottom: 0.8em;  
                                 color: white;" 
-				onclick="count(document.getElementsByName('meal'), document.getElementById('s1').value, document.getElementById('s2').value, document.getElementById('s3').value, document.getElementById('patients').value, document.getElementById('dp').value, document.getElementById('staff').value)">Calculate</button>
+								onclick="count(document.getElementsByName('meal'), document.getElementById('s1').value, document.getElementById('s2').value, document.getElementById('s3').value, document.getElementById('patients').value, document.getElementById('dp').value, document.getElementById('staff').value)">Calculate</button>
                     </center>
                 </div>
-		<div class="middle_right" style = "width: 50%;" >
+		<div class="middle_right" >
 			<center><label style="background-color: #CFD8DC" align="center" id="lb1" ></label></center><br>
 		</div>
 		</div>
@@ -430,11 +377,12 @@
                                     meal1 = meal[i].value;
                                 }
                             }
-				checkBrowser();
-				alert("awaaaaa");
+
+
+                            checkBrowser();
                             obj.onreadystatechange = function () {
 
-                                alert(obj.responseText;);
+                                //alert(obj.responseText);
                                 if (obj.readyState === 4 && obj.status === 200) {
                                     var text = obj.responseText;
                                     document.getElementById('lb1').innerHTML = text;
@@ -489,7 +437,47 @@
                         catch (err) {
                         }
                     }
-        
+                </script>
+	<script type="text/javascript">
+	function validateForm(){
+      
+         if( document.new_count.s1.value == "" )
+         {
+            alert( "Please provide the count!" );
+            document.new_count.s1.focus() ;
+            return false;
+         }
+		 if( document.new_count.s2.value == "" )
+         {
+            alert( "Please provide the count!" );
+            document.new_count.s2.focus() ;
+            return false;
+         }
+		 if( document.new_count.s3.value == "" )
+         {
+            alert( "Please provide the count!" );
+            document.new_count.s3.focus() ;
+            return false;
+         }
+		 if( document.new_count.norm_patients.value == "" )
+         {
+            alert( "Please provide the count!" );
+            document.new_count.norm_patients.focus() ;
+            return false;
+         }
+		 if( document.new_count.dd_patients.value == "" )
+         {
+            alert( "Please provide the count!" );
+            document.new_count.dd_patients.focus() ;
+            return false;
+         }
+		 if( document.new_count.staff.value == "" )
+         {
+            alert( "Please provide the count!" );
+            document.new_count.staff.focus() ;
+            return false;
+         }
+	}
 	</script>
 	
  </body>
