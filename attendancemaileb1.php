0cb58b48b6c3b579e2f44c50560bbd109e73708b
ramba93 <munasinghetd@gmail.com>
2017-01-13 11:06:43 +0530

    includes

--- attendancemaileb1.php
+++ attendancemaileb1.php
@@ -224,13 +224,13 @@
             <div id="pop_box_att_1">
                   <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">ATTENDANCE </a></div>
                   <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
-                                    <?php//  include 'attendence/doctor.php'; ?> 
+                                    <?php  include 'attendence/doctor.php'; ?> 
                   </div>
             </div>
             <div id="pop_box_att_2">
                   <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">LEAVE </a></div>
                   <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
-                                    <?php  echo 'include here'; ?> 
+                                    <?php   include 'leave/doctorLeave.php';?> 
                   </div>
             </div>
             <div id="pop_box_report_1">
