a6154d7b917faf9a89fc2835dddc7191874c59d7
ramba93 <munasinghetd@gmail.com>
2017-01-14 23:28:20 +0530

    Update attendancemaileb1.php

--- attendancemaileb1.php
+++ attendancemaileb1.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Ateendace EB 1</title>
+<title>Atendence EB 1(Doctor)</title>
 <meta charset=utf-8 />
 <link rel="stylesheet" type="text/css" href="styles/global.css" />
 	<link rel="stylesheet" type="text/css" href="del/styles.css" />
@@ -288,7 +288,7 @@
             <div id="pop_box_report_1">
                   <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">EMPLOYEE DETAILS </a></div>
                   <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
-                                    <?php include 'hrreport.php' ?> 
+                                    <?php include 'hrreport.php'?> 
                   </div>
             </div>
             <div id="pop_box_att_2">
