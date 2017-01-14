70ea68d1db8cc5d5788f99b70754bbbc85ee0b84
Bhasura Suseen Gunawardhana <bhasuras@live.com>
2017-01-14 01:57:02 -0800

    attendence mail 2

--- attendancemaileb2.php
+++ attendancemaileb2.php
@@ -318,7 +318,7 @@ else{
             });
             $('#open_att_2').click(function(){
                 $('#pop_background').fadeIn();
-                $('#pop_box_hr_2').fadeIn();
+                $('#pop_box_att_2').fadeIn();
                 return false;
             });
             $('#open_report_1').click(function(){
