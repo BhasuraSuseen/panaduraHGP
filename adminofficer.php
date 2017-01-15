b7b6b94987c806501c40bc4183a21bbf4a455f5a
SaMv <sameeranc11@gmail.com>
2017-01-15 11:58:57 +0530

    spellings

--- adminofficer.php
+++ adminofficer.php
@@ -160,7 +160,7 @@ header("Location:  index.php");
             <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;"> LETTER UPDATE </a></div>
                 <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
         			<form name="mstoao" action="adminofficer.php" method="post" accept-charset="utf-8">
-        				<b>Enter MS to AO date</b></br><br>
+        				<b>Enter Medical Supirintendent to Admin Officer date</b></br><br>
         				  <label>Letter ID</label>
         						  <?php  
         				     			require "connect.php";
