c6f390fd009e296fbe3d06b408123bd2cce5f283
ramba93 <munasinghetd@gmail.com>
2017-01-15 11:29:14 +0530

    Update attendancemaileb4.php

--- attendancemaileb4.php
+++ attendancemaileb4.php
@@ -291,13 +291,13 @@
             </div>
             <div id="pop_box_report_1">
                   <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">EMPLOYEE DETAILS </a></div>
-                  <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
+                  <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); overflow-y: scroll"> 
                                     <?php  include 'hrreport.php' ?> 
                   </div>
             </div>
             <div id="pop_box_att_1">
                   <div style="width: 100%; background-color: #2980b9;"><a style="font-size: 16px; color: #fafafa; padding: 10px;">ATTENDANCE </a></div>
-                  <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
+                  <div style="width:100%;background: #fff; padding: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); overflow-y: scroll"> 
                                     <?php  include 'attendence/minor.php'; ?>  
                   </div>
             </div>
