b36dbf1007c143daf3885dcc1398dd341c84d112
ramba93 <munasinghetd@gmail.com>
2017-01-15 12:41:04 +0530

    Update eb.php

--- attendence/eb.php
+++ attendence/eb.php
@@ -22,7 +22,7 @@
                     <div class="attrecordstyl">
                             
                         <table border="0" style="width: 100%">
-                            <tr><td style="width:40%">Day:</td><td><?php echo $yearmon;?><select  class="attstyled-select" id="day1">
+                        <tr><td style="width:40%">Day</td><td><?php echo $yearmon;?><select class="attstyled-select" style="width:30%" id="day1">
 
 
                                         <?php
@@ -41,6 +41,7 @@
                                         ?>
 
                                 </td></tr>
+
                             <tr><td style="width:70%">Employees Present </td><td><strong id="st1"></strong></td></tr>
                             <tr><td style="width:70%">Employees absent </td><td><strong id="st2"></strong></td></tr>
                             <tr><td style="width:70%">Total Employees </td><td><strong id="st3"></strong></td></tr>
