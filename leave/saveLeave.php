3050eae76214c08caeb5e3d1c7c6790d98c6f7c8
ramba93 <munasinghetd@gmail.com>
2017-01-15 21:06:37 +0530

    Update saveLeave.php

--- leave/saveLeave.php
+++ leave/saveLeave.php
@@ -13,7 +13,7 @@ echo $nic.",".$LvType.",".$des.",".$rtype.",".$date3;
 //    mysqli_query($server, "insert into attendence(leav,leav_type,req_by) values ('$leave','$type','$rtype') where empid = '$nic' and date = '$date3' ");
  $res=mysqli_query($conn, "SELECT * FROM `leave` WHERE `leave`.date='$date3' AND `leave`.nic='$nic'");
    if($row=mysqli_fetch_array($res)){
-   if($row[0]=="" && $row[1]==""){
+   if($row[0]==Null && $row[1]==Null){
     mysqli_query($conn, "insert INTO `leave` VALUES('$date3','nic','LvType','$des','$rtype')");
    echo 'Leave record Added!';
    } 
