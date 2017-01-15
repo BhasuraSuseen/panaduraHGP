b3f3467078372a434924081a336d67da54ce61c3
ramba93 <munasinghetd@gmail.com>
2017-01-16 01:29:33 +0530

    para report

---
+++ leave/LreportPara.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'datetime.php';
+//include 'connection.php';
+?>
+
+
+<?php
+
+//echo 'Panadura base hospital';
+//echo "<br>";
+//echo 'Month attendence report                                                                                                   ';
+//echo $yearmon;
+
+require 'pdfcrowd.php';
+try
+{   
+    // create an API client instance
+    $client = new Pdfcrowd("homs1", "f8f24f27034ff2118a45c9eb6ae6b49c");
+
+    // convert a web page and store the generated PDF into a $pdf variable
+    //methana thmi link ekacedit kranna one--!!!!
+    $pdf = $client->convertURI('http://omspanadura.azurewebsites.net/leave/viewLrpPara.php/');
+
+    // set HTTP response headers
+    header("Content-Type: application/pdf");
+    header("Cache-Control: max-age=0");
+    header("Accept-Ranges: none");
+    header("Content-Disposition: attachment; filename=\"Para Medical Staff Leave Report($date3).pdf\"");
+
+    // send the generated PDF 
+    echo $pdf;
+}
+catch(PdfcrowdException $why)
+{
+    echo "Pdfcrowd Error: " . $why;
+}
+?>
+    
---
+++ leave/viewLrpPara.php
@@ -0,0 +1,45 @@
+
+47 lines (36 sloc) 1.34 KB
+
+
+<?php
+include 'datetime.php';
+include '../connect.php';
+$res = mysqli_query($conn, "SELECT  employee.NIC_NO,
+  employee.F_Name,
+  employee.L_Name,
+  employee.E_type,
+	`leave`.leave_type,
+	`leave`.description,
+	`leave`.req_type
+	
+FROM employee
+  INNER JOIN `leave`
+    ON `leave`.nic = employee.NIC_NO where `leave`.date = '$date3' and employee.E_type='para'"); //and attendence = 0
+?>
+<center>
+	<h3>Panadura Base Hospital</h3>
+	<h3><?php echo $date3;?>Para Medical Staff Leave report</h3>
+<table border = '1' style='width: 90%' id="lvtbl" name="lvtbl" >
+    <tr style='background-color: appworkspace' >
+        <td style="background-color: #CFD8DC">NIC</td>
+        <td style="background-color: #CFD8DC">Name</td>
+        <td style="background-color: #CFD8DC">Leave Type</td>
+        <td style="background-color: #CFD8DC">Description</td>
+        <td style="background-color: #CFD8DC">Requre Type</td></tr>
+
+<?php while ($row = mysqli_fetch_array($res)) :; ?>
+    <tr><td><?php echo $row[0];?></td>
+        <td><?php echo $row['F_Name'] . " " . $row['L_Name'];?></td>
+        <td><?php echo $row[4];?></td>
+        <td><?php echo $row[5];?></td>
+        <td><?php echo $row[6];?></td></tr>
+<?php endwhile;?>
+</table>
+
+</center>
+<br>
+<br>
+_______________________<br>
+      Signature
+    <script type="text/javascript" src="../jst.js"></script>
