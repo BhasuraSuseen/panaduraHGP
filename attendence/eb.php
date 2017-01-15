6aff92660adb24fa70386895d142872b8ed635b5
sajini94 <sajini.rathnayake@gmail.com>
2017-01-15 11:17:52 +0530

    Update eb.php

--- attendence/eb.php
+++ attendence/eb.php
@@ -12,9 +12,9 @@
             <div class="attcontainer">
                 <div class="attmiddle_left">
                     <div style="height: 650px">
-                
+                <div style="overflow-y: scroll">
                             <?php include 'ebatt.php'; ?>
-                       
+                       </div>
                     </div>
                 </div>
                 <div class="attmiddle_right">
