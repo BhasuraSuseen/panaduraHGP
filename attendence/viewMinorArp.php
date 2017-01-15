47fd520971cafbd8d7d5aa44198d952cc676524b
ramba93 <munasinghetd@gmail.com>
2017-01-15 12:33:28 +0530

    Update viewMinorArp.php

--- attendence/viewMinorArp.php
+++ attendence/viewMinorArp.php
@@ -3,14 +3,14 @@
 include 'datetime.php';
 include 'connect.php';
 
-
+$date = $_POST['day'];
 $presentqury =mysqli_query($conn, "SELECT
   COUNT(attend.$day)
 FROM attend
   INNER JOIN employee
     ON attend.Employee_Nic = employee.Nic_no 
 WHERE employee.E_type = 'minor' and
-attend.$day = '1' and attend.date = '$yearmon'");
+attend.$date = '1' and attend.date = '$yearmon'");
 
 while($row = mysqli_fetch_array($presentqury)){
       $present = $row[0];
@@ -24,7 +24,7 @@ FROM attend
   INNER JOIN employee
     ON attend.Employee_Nic = employee.Nic_no 
 WHERE employee.E_type = 'minor' and
-attend.$day = '0' and attend.date = '$yearmon'");
+attend.$date = '0' and attend.date = '$yearmon'");
 while($row1 = mysqli_fetch_array($absentqury)){
       $absent = $row1[0];
      // echo $absent." Employees Absent today.";
