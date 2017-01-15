b15006618ca2bb64753d7165e7df40ac38c5801b
ramba93 <munasinghetd@gmail.com>
2017-01-15 21:10:31 +0530

    Update saveLeave.php

--- leave/saveLeave.php
+++ leave/saveLeave.php
@@ -11,16 +11,16 @@ echo $nic.",".$LvType.",".$des.",".$rtype.",".$date3;
 
 
 //    mysqli_query($server, "insert into attendence(leav,leav_type,req_by) values ('$leave','$type','$rtype') where empid = '$nic' and date = '$date3' ");
- $res=mysqli_query($conn, "SELECT * FROM `leave` WHERE `leave`.date='$date3' AND `leave`.nic='$nic'");
-   if($row=mysqli_fetch_array($res)){
-   if($row[0]!=NULL && $row[1]!=NULL){
+ //$res=mysqli_query($conn, "SELECT * FROM `leave` WHERE `leave`.date='$date3' AND `leave`.nic='$nic'");
+  // if($row=mysqli_fetch_array($res)){
+  // if($row[0]!=NULL && $row[1]!=NULL){
     mysqli_query($conn, "insert INTO `leave` VALUES('$date3','nic','LvType','$des','$rtype')");
    echo 'Leave record Added!';
-   } 
-    else {
-   mysqli_query($conn, "update  `leave` SET `leave`.leave_type='LvType',`leave`.description='$des',`leave`.req_type='$rtype' `leave`.date='$date3' AND `leave`.nic='$nic' )");
-    echo 'Leave record Updated!';
+  // } 
+  //  else {
+  // mysqli_query($conn, "update  `leave` SET `leave`.leave_type='LvType',`leave`.description='$des',`leave`.req_type='$rtype' `leave`.date='$date3' AND `leave`.nic='$nic' )");
+  //  echo 'Leave record Updated!';
    
-   }
-   }
+  // }
+  // }
 ?>
