11ec346a8263ab46d15d8472ba32f20c71704082
ramba93 <munasinghetd@gmail.com>
2017-01-15 12:54:34 +0530

    Update eb.php

--- attendence/eb.php
+++ attendence/eb.php
@@ -22,7 +22,7 @@
                     <div class="recordstyl">
                             
                         <table border="0" style="width: 100%">
-                        <tr>Employee Attendence Day Report</tr>
+                        <tr>Employee Attendence Day Summary</tr>
                         <tr><td style="width:40%">Day</td><td><?php echo $yearmon;?><select class="attstyled-select" style="width:30%" id="day1">
 
 
@@ -53,7 +53,7 @@
                     <br><br>
                     <div class="recordstyl"> 
 
-                        <table border="0" style="width: 100%">
+                        <table border="1" style="width: 100%">
                             <tr>Search Employee Attendence</tr>
                             <tr><td style="width:40%">Day</td><td><?php echo $yearmon;?><select class="attstyled-select" style="width:30%" id="day">
 
