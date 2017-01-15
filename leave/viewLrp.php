38aaea0839d4cc283d33b3878d080466b73bd551
ramba93 <munasinghetd@gmail.com>
2017-01-15 20:27:35 +0530

    Add files via upload

--- leave/viewLrp.php
+++ leave/viewLrp.php
@@ -1,34 +1,5 @@
 <html>
     <body>
-    <script>
-            function tableToJson(table) {
-                var data = [];
-
-// first row needs to be headers
-                var headers = [];
-                for (var i = 0; i < table.rows[0].cells.length; i++) {
-                    headers[i] = table.rows[0].cells[i].innerHTML.toUpperCase().replace(/ /gi, '');
-                }
-                data.push(headers);
-// go through cells
-                for (var i = 1; i < table.rows.length; i++) {
-
-                    var tableRow = table.rows[i];
-                    var rowData = {};
-
-                    for (var j = 0; j < tableRow.cells.length; j++) {
-
-                        rowData[ headers[j] ] = tableRow.cells[j].innerHTML;
-
-                    }
-
-                    data.push(rowData);
-                }
-
-                return data;
-            }
-        </script>
-
 
 <?php
 
@@ -36,25 +7,33 @@ include '../datetime.php';
 
 include '../connect.php';
 
-$res = mysqli_query($conn, "SELECT  employee.Nic_no,
+$res = mysqli_query($conn, "SELECT  employee.NIC_NO,
   employee.F_Name,
   employee.L_Name,
-  attend.$day,
-  employee.E_type
+  employee.E_type,
+	`leave`.leave_type,
+	`leave`.description,
+	`leave`.req_type
+	
 FROM employee
-  INNER JOIN attend
-    ON attend.Employee_Nic = employee.Nic_no where attend.date = '$yearmon'"); //and attendence = 0
+  INNER JOIN `leave`
+    ON `leave`.nic = employee.NIC_NO where `leave`.date = '$date3'"); //and attendence = 0
 ?>
 
 <table border = '1' style='width: 70%' id="lvtbl" name="lvtbl" >
     <tr style='background-color: appworkspace' >
-        <td>ID</td>
+        <td>NIC</td>
         <td>Name</td>
         <td>Leave Type</td>
-        <td>Requed Type</td></tr>
+        <td>Description</td>
+        <td>Requre Type</td></tr>
 
 <?php while ($row = mysqli_fetch_array($res)) :; ?>
-    <tr><td><?php echo $row[0];?></td><td><?php echo $row['F_Name'] . " " . $row['L_Name'];?></td><td><?php echo $row[3];?></td><td><?php echo $row[4];?></td></tr>
+    <tr><td><?php echo $row[0];?></td>
+        <td><?php echo $row['F_Name'] . " " . $row['L_Name'];?></td>
+        <td><?php echo $row[4];?></td>
+        <td><?php echo $row[5];?></td>
+        <td><?php echo $row[6];?></td></tr>
 <?php endwhile;?>
 </table>
 
@@ -62,28 +41,6 @@ FROM employee
          <button  style="width: 150px" onclick="callme();">Download
                 </button>
 </body>
-        
-         <script> function callme() {
-                var table = tableToJson($('#lvtbl').get(0));
-                var doc = new jsPDF('1', 'pt', 'letter', true);
-
-
-                $.each(table, function(i, row) {
-                    $.each(row, function(j, cell) {
-                        if (j == "DATE" | i == 0) {
-                            doc.cell(20, 10, 200, 30, cell, i);
-                        }
-                        else {
-                            doc.cell(20, 10, 200, 30, cell, i);
-                        }
-
-                    });
-                });
-
-                doc.save('Report.pdf');
-                alert('ela bn..');
-            }
-        </script>
 
     <script type="text/javascript" src="../jspdf.js"></script>
     <script type="text/javascript" src="../jquery.js"></script>
