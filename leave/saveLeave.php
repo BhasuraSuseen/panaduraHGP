c0ddf998bdfed47ad5c894e9960e307ee06939e5
ramba93 <munasinghetd@gmail.com>
2017-01-15 23:11:29 +0530

    Update saveLeave.php

--- leave/saveLeave.php
+++ leave/saveLeave.php
@@ -14,12 +14,12 @@ echo $nic.",".$LvType.",".$des.",".$rtype.",".$date3;
  //$res=mysqli_query($conn, "SELECT * FROM `leave` WHERE `leave`.date='$date3' AND `leave`.nic='$nic'");
  // while($row=mysqli_fetch_array($res)){
   // if($row[0]!=NULL && $row[1]!=NULL){
-    mysqli_query($conn, "insert INTO `leave` VALUES('$date3','$nic','$LvType','$des','$rtype')");
-   echo 'Leave record Added!';
+  //  mysqli_query($conn, "insert INTO `leave` VALUES('$date3','$nic','$LvType','$des','$rtype')");
+  // echo 'Leave record Added!';
   // } 
   // else {
-  // mysqli_query($conn, "update  `leave` SET `leave`.leave_type='$LvType',`leave`.description='$des',`leave`.req_type='$rtype' `leave`.date='$date3' AND `leave`.nic='$nic' )");
-  //echo 'Leave record Updated!';
+   mysqli_query($conn, "update  `leave` SET `leave`.leave_type='$LvType',`leave`.description='$des',`leave`.req_type='$rtype' `leave`.date='$date3' AND `leave`.nic='$nic' )");
+  echo 'Leave record Updated!';
    
   // }
   // }
