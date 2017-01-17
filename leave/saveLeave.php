1f3ae45569bdf3b935a0def1e59b8383fde29c63
ramba93 <munasinghetd@gmail.com>
2017-01-17 09:49:53 +0530

    query

--- leave/saveLeave.php
+++ leave/saveLeave.php
@@ -9,18 +9,14 @@ $rtype = $_POST['rqType'];
 
 //echo $nic.",".$LvType.",".$des.",".$rtype.",".$date3;
 
+$res = mysqli_query($conn, "SELECT * FROM `leave` WHERE `leave`.date='$date3' AND `leave`.nic='$nic'");
+if (mysqli_fetch_array($res) == NULL) {
 
-
- //$res=mysqli_query($conn, "SELECT * FROM `leave` WHERE `leave`.date='$date3' AND `leave`.nic='$nic'");
- // while($row=mysqli_fetch_array($res)){
-  // if($row[0]!=NULL && $row[1]!=NULL){
-    mysqli_query($conn, "insert INTO `leave` VALUES('$date3','$nic','$LvType','$des','$rtype')");
-   echo 'Leave record Added!';
-  // } 
-  // else {
-  // mysqli_query($conn, "update  `leave` SET `leave`.leave_type='$lvType',`leave`.description='$des',`leave`.req_type='$rtype'  WHERE `leave`.nic='$nic' AND `leave`.date='$date3')");
- // echo 'Leave record Updated!';
-   
-  // }
-  // }
+    $query2 = mysqli_query($conn, "insert INTO `leave` VALUES('$date3','$nic','$LvType','$des','$rtype')");
+    echo 'Leave record Added!';
+} else {
+ 
+   $query3 = mysqli_query($conn, "update  `leave` SET `leave`.leave_type='$LvType',`leave`.description='$des',`leave`.req_type='$rtype' where `leave`.date='$date3' AND `leave`.nic='$nic' ");
+    echo 'Leave record Updated!';
+}
 ?>
