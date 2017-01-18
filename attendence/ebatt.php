c8dac7c09169ed0ad344316f6094a55f6fe3fbed
sajini94 <sajini.rathnayake@gmail.com>
2017-01-18 12:55:47 +0530

    Update ebatt.php

--- attendence/ebatt.php
+++ attendence/ebatt.php
@@ -1,12 +1,16 @@
 <?php
+//include necessary files
 include 'attendence/datetime.php';
 include 'connect.php';
+//query for get employee data from employee data
 $res1 = mysqli_query($conn, "SELECT * FROM employee");
+//include that data to an array
 while ($row1 = mysqli_fetch_array($res1)) {
     $nic = $row1[0];
+    //query for create table data when opening the page
     mysqli_query($conn, "insert into attend(date,employee_nic) values ('$yearmon','$nic')");
 }
-
+//query for get data from employee table and attend table by an iner join for create attendence table
 $res = mysqli_query($conn, "SELECT  employee.Nic_no,
   employee.F_Name,
   employee.L_Name,
@@ -27,7 +31,7 @@ FROM employee
         <h5 style="display: none"><?php echo $date4 ?></h5>
         <h5 style="display: none"><?php echo $day ?></h5>
         <body>
-        
+       <!--create table--> 
         <table border="0" style="width: 100%">
                 <tr>
                     <td style="background-color: #CFD8DC">ID</td>
@@ -43,16 +47,21 @@ FROM employee
                 </tr>
 
                 <tr>
+                    <!--create rows by using while and fill up the database values for it-->
 <?php while ($row = mysqli_fetch_array($res)):; ?>
+                    <!--select only EB data to enter the data into the table-->
     <?php if ($row['E_type'] == "eb") { ?>
+                   
                             <td style="width: 15%; "><input type="text" value="<?php echo $row['Nic_no']; ?>"  id="NIC" readonly></td>
                             <td style=""><input type="text" value="<?php echo $row['F_Name'] . " " . $row['L_Name']; ?>" name="name"</td>
-
+<!--active monday coloumn if the actual day is monday-->
                             <?php if ($date4 == "Monday") { ?>
 
                                 <td style="width: 8%; background-color: lightgrey">
+                                    <!--if database value is 1 then it have to be ticked-->
                                     <input type="checkbox" name="mon" <?php if ($row[3] == 1) {
                         echo 'Checked';
+    //call the save attendance function
                     } ?> onclick="saveAttends('<?php echo $row[0]; ?>', document.getElementById('date').innerHTML, this.checked)"> </td>  
                                 <td style="width: 8%; background-color: #ECEFF1">
                                     <input type="checkbox" name="tue" style="display: none" onclick="saveAttends('<?php echo $row[0]; ?>', document.getElementById('date').innerHTML, this.checked)"></td>
@@ -194,7 +203,7 @@ FROM employee
 
     </center>
 </body>
-
+<!--clude the java script files from jst.js-->
 <script type="text/javascript" src="./attendence/jst.js"></script>
 
 
