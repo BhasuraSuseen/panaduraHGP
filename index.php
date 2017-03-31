1aede42cdc64944996de69d15a498c079cc6a2d9
SaMv <sameeranc11@gmail.com>
2017-03-31 15:47:07 +0530

    cc

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
     <input  type="submit" name="submit" value ="Log In" class="button">
       </form>
-      
+ <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.     
  <?php
     require_once "connect.php";
 	session_start();
@@ -80,6 +80,6 @@
 	?>
 	</div>
 	</div>
-<div><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.</div>
+
 </body>
 </html>
